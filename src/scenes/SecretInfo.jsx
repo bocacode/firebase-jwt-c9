9d2fb25937feaf5c0d355706197a5859753a025e
Todd Albert <todd@bocacode.com>
2023-02-27 15:09:02 -0500

    getting and sending token

--- src/scenes/SecretInfo.jsx
+++ src/scenes/SecretInfo.jsx
@@ -1,12 +1,30 @@
 import { useEffect, useState } from 'react'
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCciKje8wF3cL27dStykexiUcxYecwFrIA",
+  authDomain: "fb-jwt-c9.firebaseapp.com",
+  projectId: "fb-jwt-c9",
+  storageBucket: "fb-jwt-c9.appspot.com",
+  messagingSenderId: "600375133983",
+  appId: "1:600375133983:web:acb2af143b105a3fe1d22a"
+};
 
 export default function SecretInfo() {
   const [secretStuff, setSecretStuff] = useState()
   useEffect(() => {
-    // make a secure call to our API to get the secret info
-    fetch('http://localhost:3030/secrets')
-      .then(res => res.json())
-      .then(data => setSecretStuff(data.message))
+    const app = initializeApp(firebaseConfig)
+    const auth = getAuth(app)
+    auth.currentUser.getIdToken(false)
+      .then(token => {
+        // make a secure call to our API to get the secret info
+        fetch('http://localhost:3030/secrets', {
+          headers: { Authorization: token },
+        }) // SEND TOKEN
+          .then(res => res.json())
+          .then(data => setSecretStuff(data.message))
+      })
       .catch(alert)
   }, [])
   return(
